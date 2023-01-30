bbf4f6e0405e0c4f1a6ace2498539d8fd476321e
Mariano Brarda <64959782+RevelioX@users.noreply.github.com>
2023-01-30 05:33:14 -0300

    added *{font-family]

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,17 @@
+import { Button, FormLabel, Input, Sheet } from "@mui/joy"
+import styles from "../styles/login.module.css"
+
 export default function Login(){
     return(
-        <h1> Banana Azul </h1>
+        <div className={styles.login}>
+            <Sheet className={styles.sheet} variant="outlined" color="neutral">
+            <FormLabel>Username</FormLabel>
+            <Input className={styles.login_username} type="text" placeholder="RevelioX"></Input>
+            <FormLabel>Password</FormLabel>
+            <Input className={styles.login_password} type="password"></Input>
+            <Button color="primary" disabled={false} onClick={function(){}} size="md" variant="solid" className={styles.login_button}>Login</Button>
+            </Sheet>
+        </div>
     )
 }
+
