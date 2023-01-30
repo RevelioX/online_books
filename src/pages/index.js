9f15cc808bea8634491e47a7db2b487784b27974
Themilien <99250998+Themilien@users.noreply.github.com>
2023-01-30 05:22:10 -0300

    Funete y algo de index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/index.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1>Store of Books</h1>
+      <div className={styles.title}>
 
-      <p>The best website for buy many online and phisycs books</p>
+        <h1>Store of Books</h1>
 
-      <div class="sale">
-        <img src="" alt=""/>
-        <img src="" alt=""/>
-        <img src="" alt=""/>
-        <img src="" alt=""/>
+        <p>The best website for buy many online and phisycs books</p>
+
+      </div>
+
+
+      <div className={styles.sale} >
+
+        <img className={styles.img} src="D:\Cosas Program\Github\online_books\public\index\communistbook.jpeg" alt="comunismo"/>
+        <img className={styles.img2} src="public\index\communistbook2.jpeg" alt="comunismo2"/>
+        <img className={styles.img3} src="public\index\communistbook3.jpeg" alt="comunismo3"/>
 
       </div>
     </>
