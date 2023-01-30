bc25aabb93c85002a16536bbe008a888cf786784
Themilien <99250998+Themilien@users.noreply.github.com>
2023-01-30 07:22:00 -0300

    Ajustes menores

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,5 +2,5 @@ import '@/styles/globals.css'
 import Nav from '../components/nav.js'
 
 export default function App({ Nav, Component, pageProps }) {
-  return( <Nav Component {...pageProps} />)
+  return( <Nav/> <Component {...pageProps} />)
 }
