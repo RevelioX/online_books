bd5630c248919696ade2c9401de639f4e25cfd4b
Themilien <99250998+Themilien@users.noreply.github.com>
2023-01-30 06:33:05 -0300

    Mucho index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,40 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/index.module.css'
 
+
+
+import comunismo from "../../public/index/communistbook.jpeg";
+import comunismo2 from "../../public/index/communistbook2.jpeg";
+import comunismo3 from "../../public/index/title_background.jpeg";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <div className={styles.title}>
+      <div className={styles.container}>
+        <div className={styles.title}>
 
-        <h1>Store of Books</h1>
+          <h1>Store of Books</h1>
 
-        <p>The best website for buy many online and phisycs books</p>
+          <p>The best website for buy many online and phisycs books</p>
 
+        </div>
+
+        <div className={styles.sale}>
+
+          <Image src={comunismo} className={styles.comunismo}alt="comunismo"/>
+
+          <Image src={comunismo2} className={styles.comunismo2} alt="comunismo2"/>
+          
+          <Image src={comunismo3} className={styles.comunismo3} alt="comunismo3"/>
+
+        </div>
+
+        <div className={styles.footer}>
+          <p>We are GODS, you should buy our books for your heal</p>
+        </div>
       </div>
-
-      <div className={styles.sale}>
-
-
-      </div>
-
-      <div className={styles.footer}>
-
-      </div>
-
     </>
   )
 }
