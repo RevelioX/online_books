d53bed3d35b799d942c4f0eb90dc8cb25d2b796d
Themilien <99250998+Themilien@users.noreply.github.com>
2023-01-30 06:52:35 -0300

    Más nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,20 @@ import style from "nav.css"
 export default function Navigator(){
     return(
         <nav>
-            
+            <div className={Style.search}>
+
+            </div>
+
+            <div className={Style.options}>
+
+                <div className={Style.Indice}></div>
+
+                <div className={Style.carrito}></div>
+
+                <div className={Style.Perfil}></div>
+
+
+            </div>
         </nav>
     )
 }
