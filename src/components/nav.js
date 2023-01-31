f52bff8b195466a045d3a02325124f581bba29ec
Mariano Brarda <64959782+RevelioX@users.noreply.github.com>
2023-01-31 01:18:44 -0300

    login error working

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,15 @@
 import style from "@/styles/nav.module.css"
-import { Input, Button } from "@mui/joy";
+import { Input, Button, Link } from "@mui/joy";
 
 export default function Navigator(){
     return(
-        <nav className={style.nav}>
+        <nav id="demo_dark-mode-by-default" className={style.nav}>
             <div className={style.search}>
 
                 <Input type="text" placeholder="🔎Search..."/>
+                <Link>About Us</Link>
+                <Link>Catalogo</Link>
+                
 
             </div>
             
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,26 @@
 import { Button, FormLabel, Input, Sheet } from "@mui/joy"
 import styles from "../styles/login.module.css"
+import React from "react";
+import { style } from "@mui/system";
 
 export default function Login(){
+    const [inputState,setInputState] = React.useState({username: true, password: true});
+
+    function handleClick(){
+        setInputState({username: false, password: false})
+        console.log("Azul")
+    }
+
     return(
-        <div className={styles.login}>
-            <h2>Sign In</h2>
+        <div className={styles.background}>
             <Sheet className={styles.sheet}variant="outlined" color="neutral">
+            <h2 className={styles.text}>Login</h2>
             <FormLabel className={styles.Labels}>Username</FormLabel>
-            <Input className={styles.login_username} type="text" placeholder="RevelioX"></Input>
+            <Input className={styles.login_username} type="text" placeholder="RevelioX" color={ inputState.username ? "primary" : "danger"}></Input>
             <FormLabel className={styles.Labels}>Password</FormLabel>
-            <Input className={styles.login_password} type="password"></Input>
-            <Button color="primary" size="md" variant="solid" className={styles.login_button}>Login</Button>
+            <Input className={styles.login_password} type="password" color={ inputState.password ? "primary" : "danger"}></Input>
+            {!(inputState.username||inputState.password) && <p className={styles.error}>El nombre de usuario o la contraseña son incorrectos</p>}
+            <Button color="primary" onClick={handleClick} size="sm" variant="solid" className={styles.login_button}>Login</Button>
             </Sheet>
         </div>
     )
