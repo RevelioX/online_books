a38b8b12fc254222c1ed507b4b9a7eeecab08cb7
Themilien <99250998+Themilien@users.noreply.github.com>
2023-02-04 05:56:13 -0300

    Si

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -16,21 +16,6 @@ export default function Signup(){
 
             <h2 className={styles.text}>SignUp</h2>
 
-<<<<<<< HEAD
-            <FormLabel className={styles.Labels}>Gmail</FormLabel>
-            <Input required className={styles.signup_gmail} type="text" placeholder="RevelioX" color={ inputState.username ? "primary" : "danger"}></Input>
-
-            <FormLabel className={styles.Labels}>Username</FormLabel>
-            <Input required className={styles.signup_username} type="text" placeholder="RevelioX" color={ inputState.username ? "primary" : "danger"}></Input>
-
-            <FormLabel className={styles.Labels}>Password</FormLabel>
-            <Input required className={styles.signup_password} type="password" color={ inputState.password ? "primary" : "danger"}></Input>
-
-            <FormLabel className={styles.Labels}>Confirm Password</FormLabel>
-            <Input required className={styles.signup_password_confirm} type="password" color={ inputState.password ? "primary" : "danger"}></Input>
-
-            <Button color="primary" onClick={handleClick} size="sm" variant="solid" className={styles.signup_button}>Sign Up</Button>
-=======
             <FormLabel className={styles.Labels}>Username</FormLabel>
             <Input required className={styles.login_username} type="text" placeholder="RevelioX" color={ inputState.username ? "primary" : "danger"}></Input>
             
@@ -44,7 +29,6 @@ export default function Signup(){
             <Input required className={styles.login_password} type="email" color={ inputState.password ? "primary" : "danger"}></Input>
 
             <Button color="primary" onClick={handleClick} size="sm" variant="solid" className={styles.login_button}>Sign Up</Button>
->>>>>>> 4c64b286a4d1c40966f9b2fb62704a16ca53bbcc
             </Sheet>
         </div>
     )
