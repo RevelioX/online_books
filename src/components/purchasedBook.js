312493e1e192219d102bb811b35c836fd037bec7
Themilien <99250998+Themilien@users.noreply.github.com>
2023-02-04 05:05:44 -0300

    Los elementos llevan la 1ra letra mayúscula

--- src/components/purchasedBook.js
+++ src/components/purchasedBook.js
@@ -5,10 +5,10 @@ import Image from "next/image"
 export default function purchasedBook(){
     return(
     <div className={style.compra}> 
-                        <Image src={imgcompra} className={style.compra_img}/>
-                        <p> Title: Name </p>
-                        <p> Price: $9.99 </p>
-                        <p> Buy to: Name </p>
+        <Image src={imgcompra} className={style.compra_img}/>
+        <p> Title: Name </p>
+        <p> Price: $9.99 </p>
+        <p> Buy to: Name </p>
     </div>
     )
 }
